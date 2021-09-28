7dcf6843d588a10d89383d77ae2bf5873e5b66df
S0YKIM <soyeonkim.9683@gmail.com>
2021-09-28 15:44:45 +0900

    Local Storage Added / One Counter

--- PROJECTS/counter/index.js
+++ PROJECTS/counter/index.js
@@ -1,40 +1,49 @@
+"use strict";
+
+import Counter from './counter.js';
+
 const $ = (selector) => document.querySelector(selector);
 
-let container = $('#container');
 const plusBtn = $('#plus');
 const minusBtn = $('#minus');
+const resetBtn = $('#reset');
 const number = $('span');
-const reset = $('#reset');
 
-class Counter {
-    constructor () {
-        this.count = 0;
-    }
-    increase() {
-        this.count += 1;
-        return this.count;
-    }
-    decrease() {
-        if (this.count > 0)
-            this.count -= 1;
-        return this.count;
-    }
-    reset() {
-        this.count = 0;
-        return this.count;
+const store = {
+    setLocalStorage(total) {
+        localStorage.setItem("total", String(total));
+    },
+    getLocalStorage() {
+        return localStorage.getItem("total");
     }
 }
 
-const counter = new Counter();
+function App() {
+    this.initValue = 0;
+    this.init = () => {
+        if (store.getLocalStorage()) {
+            this.initValue = Number(store.getLocalStorage());
+        }
+        number.textContent = this.initValue;
+    };
 
-plusBtn.addEventListener('click', function(){
-    number.textContent = counter.increase();
-});
+    const counter = new Counter(this.initValue);
 
-minusBtn.addEventListener('click', function(){
-    number.textContent = counter.decrease();
-});
+    plusBtn.addEventListener('click', function(){
+        number.textContent = counter.increase();
+        store.setLocalStorage(counter._count);
+    });
+    
+    minusBtn.addEventListener('click', function(){
+        number.textContent = counter.decrease();;
+        store.setLocalStorage(counter._count);
+    });
+    
+    resetBtn.addEventListener('click', function(){
+        number.textContent = counter.reset();;
+        store.setLocalStorage(counter._count);
+    });
+}
 
-reset.addEventListener('click', function(){
-    number.textContent = counter.reset();
-});
+const app = new App();
+app.init();
